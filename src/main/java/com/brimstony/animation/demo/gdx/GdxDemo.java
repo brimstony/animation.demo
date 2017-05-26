287951ad24c55482779b8df85750829ff287c00d
Tony Brimeyer <tbrimeyer@gmail.com>
2017-05-26 08:56:23 -0500

    Added up and down movement.

--- src/main/java/com/brimstony/animation/demo/gdx/GdxDemo.java
+++ src/main/java/com/brimstony/animation/demo/gdx/GdxDemo.java
@@ -19,11 +19,33 @@ public class GdxDemo extends ApplicationAdapter {
     SpriteBatch spriteBatch;
 
     // A variable for tracking elapsed time for the animation
+
+    int screenWidth;
+    int screenHeight;
+
+    float guybrushSpeed = 50.0f; // 10 pixels per second.
+    float guybrushX = 50;
+    float guybrushY = 50;
+
+    private static int STOPPED = 0;
+
+    private static int LEFT = -1;
+    private static int RIGHT = 1;
+
+    private static int DOWN = -1;
+    private static int UP = 1;
+
+    private int currentXDirection;
+    private int currentYDirection;
+
     float stateTime;
 
     @Override
     public void create() {
+        Gdx.gl.glClearColor(1.0f, 1.0f, 1.0f, 1);
 
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
         // Load the sprite sheet as a Texture
         guybrushSheet = new Texture(Gdx.files.internal("gb_walk.png"));
 
@@ -43,6 +65,7 @@ public class GdxDemo extends ApplicationAdapter {
         walkLeftAnimation = new Animation<>(0.1f, walkLeft);
 
         currentAnimation = walkLeftAnimation;
+        currentXDirection = LEFT;
 
         // Instantiate a SpriteBatch for drawing and reset the elapsed animation
         // time to 0
@@ -60,11 +83,18 @@ public class GdxDemo extends ApplicationAdapter {
                 switch (keycode) {
                     case Input.Keys.LEFT:
                         currentAnimation = walkLeftAnimation;
+                        currentXDirection = LEFT;
                         return true;
                     case Input.Keys.RIGHT:
                         currentAnimation = walkRightAnimation;
+                        currentXDirection = RIGHT;
                         return true;
-
+                    case Input.Keys.UP:
+                        currentYDirection = UP;
+                        return true;
+                    case Input.Keys.DOWN:
+                        currentYDirection = DOWN;
+                        return true;
                 }
                 return false;
             }
@@ -73,14 +103,27 @@ public class GdxDemo extends ApplicationAdapter {
 
     @Override
     public void render() {
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
         stateTime += Gdx.graphics.getDeltaTime(); // Accumulate elapsed animation time
+
+        if(currentXDirection == LEFT){
+            guybrushX -=  Gdx.graphics.getDeltaTime() * guybrushSpeed;
+        } else if (currentXDirection == RIGHT){
+            guybrushX +=  Gdx.graphics.getDeltaTime() * guybrushSpeed;
+        }
+
+        if(currentYDirection == DOWN){
+            guybrushY -=  Gdx.graphics.getDeltaTime() * guybrushSpeed;
+        } else if (currentYDirection == UP){
+            guybrushY +=  Gdx.graphics.getDeltaTime() * guybrushSpeed;
+        }
+
 
         // Get current frame of animation for the current stateTime
 
         spriteBatch.begin();
-        spriteBatch.draw(currentAnimation.getKeyFrame(stateTime, true), 50, 50); // Draw current frame at (50, 50)
-        //spriteBatch.draw(walkLeftAnimation.getKeyFrame(stateTime, true), 250, 50); // Draw current frame at (50, 50)
+        spriteBatch.draw(currentAnimation.getKeyFrame(stateTime, true), guybrushX, guybrushY);
         spriteBatch.end();
     }
 
